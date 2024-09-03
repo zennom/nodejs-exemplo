210efea9041d1b2b1c88444c09e44f2106c99de0
VIVIANE DE LIMA FRANCELINO <viviane.lfrancelino@senacsp.edu.br>
2024-09-03 08:58:27 -0300

    adicionando mais rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,4 +6,14 @@ router.get('/',(req:Request, res:Response) =>{
     res.send('Olá Mundo!')
 })
 
+router.get('/contato',(req:Request, res:Response) =>{
+    res.send('Formulário de contato')
+})
+
+router.get('/sobre',(req:Request, res:Response) =>{
+    res.send('sobre nós')
+})
+
+
+
 export default router
